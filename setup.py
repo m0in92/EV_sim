7fda2951419e36f65ad3b5135ea1be63a5acb8cf
m0in92 <moinahmed100@gmail.com>
2023-06-06 16:21:26 -0400

    updated setup.py

--- setup.py
+++ setup.py
@@ -10,6 +10,6 @@ setup(name='EV_sim',
       long_description=long_description,
       author="Moin Ahmed",
       author_email="moinahmed100#gmail.com",
-      packages=['EV_sim', 'EV_sim.examples', 'EV_sim.config'],
+      packages=['EV_sim', 'EV_sim.examples', 'EV_sim.config', 'tests'],
       install_requires=['numpy', 'pandas', 'matplotlib'],
       )
